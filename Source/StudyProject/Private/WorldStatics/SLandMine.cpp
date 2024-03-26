5dbaccc0e082f6ffbbbd81a8cf7eb0ffa39dca03
MATTTTJ <hoon3022@naver.com>
2024-03-27 01:06:37 +0900

    feat: HasAuthority 함수 활용한 로직 추가, 지뢰 Overlap 할 때 폭발 이펙트, 검은색 머테리얼 변경 구현, net culling distance squared 로 인한 동기화 문제를 Replicate, Rep_Notify로 해결

--- Source/StudyProject/Private/WorldStatics/SLandMine.cpp
+++ Source/StudyProject/Private/WorldStatics/SLandMine.cpp
@@ -1,12 +1,17 @@
 // SLandMine.cpp
 
-
 #include "WorldStatics/SLandMine.h"
 #include "Kismet/KismetSystemLibrary.h"
 #include "Components/BoxComponent.h"
 #include "Components/StaticMeshComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "Net/UnrealNetwork.h"
+#include "Particles/ParticleSystemComponent.h"
+#include "Net/UnrealNetwork.h"
+#include "Engine/Engine.h"
 
 ASLandMine::ASLandMine()
+	: bIsExploded(false)
 {
 	PrimaryActorTick.bCanEverTick = true;
 
@@ -15,18 +20,121 @@ ASLandMine::ASLandMine()
 
 	BodyStaticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BodyStaticMeshComponent"));
 	BodyStaticMeshComponent->SetupAttachment(GetRootComponent());
+
+	ParticleSystemComponent = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("ParticleSystemComponent"));
+	ParticleSystemComponent->SetupAttachment(GetRootComponent());
+	ParticleSystemComponent->SetAutoActivate(false);
 }
 
 void ASLandMine::BeginPlay()
 {
 	Super::BeginPlay();
 
-	UKismetSystemLibrary::PrintString(this, FString::Printf(TEXT("ASLandMine::BeginPlay() has been called.")));
+	//UKismetSystemLibrary::PrintString(this, FString::Printf(TEXT("ASLandMine::BeginPlay() has been called.")));
+
+	// 서버에서 실행중인지? 
+	if(true == HasAuthority())
+	{
+		UKismetSystemLibrary::PrintString(this, FString::Printf(TEXT("ASLandMine::BeginPlay() has been called in Server PC.")));
+	}
+	else
+	{
+		if(GetOwner() == UGameplayStatics::GetPlayerController(this,0))
+		{
+			UKismetSystemLibrary::PrintString(this, FString::Printf(TEXT("ASLandMine::BeginPlay() has been called in OwningClient PC.")));
+		}
+		else
+		{
+			UKismetSystemLibrary::PrintString(this, FString::Printf(TEXT("ASLandMine::BeginPlay() has been called in OtherClient PC.")));
+		}
+	}
+
+	if(false == OnActorBeginOverlap.IsAlreadyBound(this, &ThisClass::OnLandMineBeginOverlap))
+	{
+		OnActorBeginOverlap.AddDynamic(this, &ThisClass::OnLandMineBeginOverlap);
+	}
+}
+
+void ASLandMine::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	Super::EndPlay(EndPlayReason);
+
+	//UKismetSystemLibrary::PrintString(this, FString::Printf(TEXT("ASLandMine::EndPlay() has been called.")));
+	if (true == HasAuthority())
+	{
+		UKismetSystemLibrary::PrintString(this, FString::Printf(TEXT("ASLandMine::EndPlay() has been called in Server PC.")));
+	}
+	else
+	{
+		if (GetOwner() == UGameplayStatics::GetPlayerController(this, 0))
+		{
+			UKismetSystemLibrary::PrintString(this, FString::Printf(TEXT("ASLandMine::EndPlay() has been called in OwningClient PC.")));
+		}
+		else
+		{
+			UKismetSystemLibrary::PrintString(this, FString::Printf(TEXT("ASLandMine::EndPlay() has been called in OtherClient PC.")));
+		}
+	}
+
+	if(true == OnActorBeginOverlap.IsAlreadyBound(this, & ThisClass::OnLandMineBeginOverlap))
+	{
+		OnActorBeginOverlap.RemoveDynamic(this, &ThisClass::OnLandMineBeginOverlap);
+	}
 }
 
 void ASLandMine::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+}
+
+void ASLandMine::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(ThisClass, bIsExploded);
+}
+
+void ASLandMine::OnLandMineBeginOverlap(AActor* OverlappedActor, AActor* OtherActor)
+{
+	if (true == HasAuthority())
+	{
+		UKismetSystemLibrary::PrintString(this, FString::Printf(TEXT("ASLandMine::OnLandMineBeginOverlap() has been called in Server PC.")));
+	}
+	else
+	{
+		if (GetOwner() == UGameplayStatics::GetPlayerController(this, 0))
+		{
+			UKismetSystemLibrary::PrintString(this, FString::Printf(TEXT("ASLandMine::OnLandMineBeginOverlap() has been called in OwningClient PC.")));
+		}
+		else
+		{
+			UKismetSystemLibrary::PrintString(this, FString::Printf(TEXT("ASLandMine::OnLandMineBeginOverlap() has been called in OtherClient PC.")));
+		}
+	}
+	
+	if (true == HasAuthority() && false == bIsExploded)
+	{
+		SpawnEffect_NetMulticast();
+		bIsExploded = true;
+	}
+}
+
+void ASLandMine::OnRep_IsExploded()
+{
+	if(true == bIsExploded)
+	{
+		BodyStaticMeshComponent->SetMaterial(0, ExplodedMaterial);
+	}
+}
+
+void ASLandMine::SpawnEffect_NetMulticast_Implementation()
+{
+	ParticleSystemComponent->SetActive(true);
+
+	if(true == ::IsValid(ExplodedMaterial))
+	{
+		BodyStaticMeshComponent->SetMaterial(0, ExplodedMaterial);
+	}
 }
 
--- Source/StudyProject/Public/WorldStatics/SLandMine.h
+++ Source/StudyProject/Public/WorldStatics/SLandMine.h
@@ -1,4 +1,4 @@
-// SLandMine.h
+// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -6,6 +6,9 @@
 #include "GameFramework/Actor.h"
 #include "SLandMine.generated.h"
 
+/**
+ *
+ */
 UCLASS()
 class STUDYPROJECT_API ASLandMine : public AActor
 {
@@ -16,10 +19,19 @@ public:
 
 protected:
 	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
 
 public:	
 	virtual void Tick(float DeltaTime) override;
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	UFUNCTION()
+	void OnLandMineBeginOverlap(AActor* OverlappedActor, AActor* OtherActor);
 
+	UFUNCTION(NetMulticast, Unreliable)
+	void SpawnEffect_NetMulticast();
+
+	UFUNCTION()
+	void OnRep_IsExploded();
 private:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "ASLandMine", Meta = (AllowPrivateAccess))
 	TObjectPtr<class UBoxComponent> BodyBoxComponent;
@@ -27,4 +39,12 @@ private:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "ASLandMine", Meta = (AllowPrivateAccess))
 	TObjectPtr<class UStaticMeshComponent> BodyStaticMeshComponent;
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category =  "ASLandMine", Meta = (AllowPrivateAccess))
+	TObjectPtr<class UParticleSystemComponent> ParticleSystemComponent;
+
+	UPROPERTY(ReplicatedUsing = OnRep_IsExploded, VisibleAnywhere, BlueprintReadOnly, Category = "ASLandMine", Meta = (AllowPrivateAccess))
+	uint8 bIsExploded : 1;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "ASLandMine", Meta = (AllowPrivateAccess))
+	TObjectPtr<class UMaterial> ExplodedMaterial;
 };
